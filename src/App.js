25c651d8e18a23b80ce12e2b3c924414fc2e0f34
bharti zade <bhartigzade@gmail.com>
2023-03-11 07:29:31 +0530

    tast3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [name, setName] = useState("Amit");
+
+  const handleClick = () => {
+    setName(name === "Amit" ? "Rajan" : "Amit");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>My name is  {name}</h1>
+      <button className="toggle-btn" onClick={handleClick}>
+        Change Name
+      </button>
     </div>
   );
 }
-
-export default App;
